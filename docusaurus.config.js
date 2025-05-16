283828a6942166966b4adf3c3f13d5a2103b49df
NOPESbucket <zoleg7138@gmail.com>
2025-05-16 09:55:31 +0300

    Added local search engine plugin.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -21,7 +21,7 @@ const config = {
   baseUrl: '/RetroBoxDocs/',
 
   // Plugins to load
-  plugins: ['@docusaurus/theme-live-codeblock'],
+  plugins: ['@docusaurus/theme-live-codeblock', require.resolve('docusaurus-lunr-search')],
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
